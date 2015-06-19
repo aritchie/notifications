c6e3b5317634eb2522cf93743b8d2df6b1ad89d9
Allan Ritchie <allan.ritchie@gmail.com>
2015-06-19 09:07:25 -0400

    Release

--- Acr.Notifications.Shared/Notifications.cs
+++ Acr.Notifications.Shared/Notifications.cs
@@ -5,10 +5,10 @@ namespace Acr.Notifications {
 
     public static class Notifications {
         private static readonly Lazy<INotifications> instanceInit = new Lazy<INotifications>(() => {
-#if __PLATFORM__
-            return new NotificationsImpl();
-#else
+#if PCL
             throw new ArgumentException("Platform implementation not found.  Did you reference the nuget package in your main project as well?");
+#else
+            return new NotificationsImpl();
 #endif
         }, false);
 
